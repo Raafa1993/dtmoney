b150b256220b41c9297deab27fe9bddcd946abdf
Raafa1993 <rafaelajds@gmail.com>
2021-04-14 11:35:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Ola dtmoney</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
